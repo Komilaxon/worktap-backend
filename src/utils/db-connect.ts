0888658425de5d3af19fa9098bfa0cbcf42f9cfd
Komilaxon <komilafayziyeva94@gmail.com>
2024-01-11 21:50:06 +0500

    .

--- src/utils/db-connect.ts
+++ src/utils/db-connect.ts
@@ -1,8 +1,8 @@
 import mongoose from "mongoose";
-const SHARIF_URL = "mongodb+srv://fullstackdeveloper4413:yv4UCb8yhWtrbOFp@mydb.svis6ii.mongodb.net/komila?retryWrites=true&w=majority"
+// const SHARIF_URL = "mongodb+srv://fullstackdeveloper4413:yv4UCb8yhWtrbOFp@mydb.svis6ii.mongodb.net/komila?retryWrites=true&w=majority"
 export const dbConnection = async () => {
   try {
-    await mongoose.connect(SHARIF_URL);
+    await mongoose.connect("mongodb://127.0.0.1:27017/exam");
     console.log("db connected successfully...");
   } catch (error: any) {
     console.error(error);
